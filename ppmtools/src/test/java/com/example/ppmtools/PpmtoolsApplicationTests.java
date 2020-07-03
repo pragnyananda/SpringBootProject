5937e272535dcfbbfdee687ea9ffcb00b7b2d7ff
pragnyananda <45530046+pragnyananda@users.noreply.github.com>
2020-07-03 12:13:14 +0530

    Structure commit
    Committed the first structure

---
+++ ppmtools/src/test/java/com/example/ppmtools/PpmtoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppmtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
