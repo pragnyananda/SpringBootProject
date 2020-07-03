1466473be696c2dab89f3cf83161674b561ec8f6
pragnyananda <pragnyananda.mishra@gmail.com>
2020-07-03 12:11:41 +0530

    structure commit

---
+++ ppmtools/src/main/java/com/example/ppmtools/PpmtoolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ppmtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolsApplication.class, args);
+	}
+
+}
